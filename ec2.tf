49341abb4914ccac666340c0166da9b18c1f83df
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-11-28 14:35:28 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,31 +1,32 @@
-resource "aws_instance" "HelloWorld" {
+resource "aws_instance" "terraform" {
   ami                    = "ami-09c813fb71547fc4f"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all_terraform.id]
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name = "Terraform"
+    Name      = "terraform-1"
+    Terraform = "true"
   }
 }
 
-resource "aws_security_group" "allow_all_terraform" {
-  name = "Allow-All"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
+resource "aws_security_group" "allow_all" {
+  name = "allow-all"
 
   egress {
-    from_port   = 0
+    from_port   = 0 # from port 0 to to port 0 means all ports
     to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # internet
   }
 
-  tags = aws_instance.HelloWorld.tags
+  ingress {
+    from_port   = 0 # from port 0 to to port 0 means all ports
+    to_port     = 0
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "allow-all"
+  }
 
 }
