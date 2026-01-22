3503f2e007dc3979a8fee7d3589fe2928a8afa99
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-22 12:09:33 +0530

    terraform

--- dynamic/provider.tf
+++ dynamic/provider.tf
@@ -1,21 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.27.0"
+      source = "hashicorp/aws"
+      version = "6.16.0"
     }
   }
 
-
   backend "s3" {
-    bucket       = "remote-state-suresh86s"
-    key          = "provisioners-demo"
-    region       = "us-east-1"
+    bucket = "remote-state-86s"
+    key    = "dynamic-block-demo"
+    region = "us-east-1"
     use_lockfile = true
-    encrypt      = true
+    encrypt = true
   }
 }
+
 provider "aws" {
   region = "us-east-1"
-  # Configuration options
 }
--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,21 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.27.0"
+      source = "hashicorp/aws"
+      version = "6.16.0"
     }
   }
-
-
-  backend "s3" {
-    bucket       = "remote-state-suresh86s"
-    key          = "provisioners-demo"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
 }
+
 provider "aws" {
-  region = "us-east-1"
   # Configuration options
 }
