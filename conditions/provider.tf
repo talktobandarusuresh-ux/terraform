9cd317fc66989740fae7df077c8e6e88a8e8f118
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-28 17:39:57 +0530

    Terraform

--- conditions/provider.tf
+++ conditions/provider.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.16.0"
     }
+  }
+  backend "s3" {
+    bucket       = "remote-state-bandaru"
+    key          = "terraform-count"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
