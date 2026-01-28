adb2e29767f8162c68d7f8afc5e04ca26ecd78f8
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-28 18:32:04 +0530

    Terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -22,10 +22,10 @@ output "ami_id" {
   value = data.aws_ami.joindevops.id
 }
 
-data "aws_instance" "mongodb" {
-  instance_id = "i-0355f99cd46d41298"
-}
+# data "aws_instance" "mongodb" {
+#   instance_id = "i-0355f99cd46d41298"
+# }
 
-output "mongodb_info" {
-  value = data.aws_instance.mongodb.public_ip
-}
+# output "mongodb_info" {
+#   value = data.aws_instance.mongodb.public_ip
+# }
